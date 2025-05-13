40ae6168eb1c5452f5256ead349081fbf7af22bb
BrunoEvangelista17 <brunoevangelistag15@gmail.com>
2025-05-13 14:39:00 -0300

    Update MongoDB connection string and correct user email in test requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,9 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/'),
+    MongooseModule.forRoot(
+      'mongodb+srv://brunoevangelistag15:ddMZrpdZovFXY8Lh@cluster0.db5jm.mongodb.net/',
+    ),
     ProjectModule,
     TaskModule,
     UserModule,
